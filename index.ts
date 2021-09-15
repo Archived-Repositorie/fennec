7b1c8227fe0199ade25bf1fc9a9f9a54e9387fde
Cherry <73485099+JustFoxx@users.noreply.github.com>
2021-09-15 16:53:14 +0000

    added event handler

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
+//import
 import { Client, Intents } from "discord.js"
 import { config } from "./config"
-import { readFileSync } from "fs"
+import { readFileSync, readdirSync } from "fs"
 
+//settings
 const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const pfpAscii = readFileSync("./asciiIcon.ans").toString()
+
+//ctx
+class ctx {
+    constructor 
+}
+
+//event handler
+const eventFiles = readdirSync('./events')
+    .filter(file => file.endsWith('.js'));
+
+
+for(const file of eventFiles) {
+    const event = require(`./events/${file}`)
+
+    if (!event.once) {
+        client.on(event.name, async (...args) => await event.execute(...args, new ctx))
+    } else {
+        client.once(event.name, async (...args) =>  await event.execute(...args, new ctx))
+    }
+
+} 
 
 client.on("ready", async () => {
     const clientInfo = {
@@ -18,3 +41,5 @@ client.on("ready", async () => {
 })
 
 client.login(config.token)
+
+
